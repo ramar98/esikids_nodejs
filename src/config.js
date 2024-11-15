320768fccdecaa780dad20af5facdb703cb3f782
Ramiro Gonzalez <ramiro.gonzalez@grupocarreras.com>
2024-11-15 16:36:31 -0300

    primerDespliegueAhorasiv3

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-//export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 8080;
 export const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "SwtNhdYMFyatIrWhfjCbAegCZYRAJWIv";
