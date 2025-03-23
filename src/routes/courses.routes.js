ef389cf42266bd4a4e0cf9bd3a2bca6a515766e0
Ramiro Gonzalez <ramiro.gonzalez@grupocarreras.com>
2025-03-23 17:21:47 -0300

    ReportsV1

--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -5,6 +5,7 @@ import {
     getCourseById,
     deleteCourse,
     updateCourse,
+    getStudentsCompletedAllGames
 } from "../controllers/courses.controller.js";
 import { verifyToken } from "../middlewares/authJwt.js";
 const router = Router();
@@ -14,5 +15,6 @@ router.post('/courses/createCourse', verifyToken, createCourse);
 router.get('/courses/:course_id', verifyToken, getCourseById);
 router.delete('/courses/:course_id', verifyToken, deleteCourse);
 router.put('/courses/updateCourse', verifyToken, updateCourse);
+router.get('/courses/getStudentsCompletedAllGames/:course_id', verifyToken, getStudentsCompletedAllGames);
 
 export default router;
