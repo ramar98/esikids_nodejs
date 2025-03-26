d240e3e451de7e66b70721b9d7259faec0d35e7c
Ramiro Gonzalez <ramiro.gonzalez@grupocarreras.com>
2025-03-25 23:30:38 -0300

    ReportsV1

--- src/routes/users_chatbot.routes.js
+++ src/routes/users_chatbot.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {
     createUserChatbot,
-    getUsersChatbot
+    getUsersChatbot,
+    getUsersChatbotByCourse
 } from "../controllers/users_chatbot.controller.js";
 import { verifyToken } from "../middlewares/authJwt.js";
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post("/users_chatbot", verifyToken, createUserChatbot);
 router.get("/users_chatbot", verifyToken, getUsersChatbot);
+router.get("/users_chatbot/:course_id", verifyToken, getUsersChatbotByCourse);
 
 export default router;
